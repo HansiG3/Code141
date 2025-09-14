b60dbc440b54e7db90f15ca5ce9110d8ee4b7c93
HansiG3 <hansigupta3@gmail.com>
2025-09-14 17:27:42 +0530

    initial

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import LandingPage from "./Pages/LandingPage/Landingpage.jsx";
-
-
+import LandingPage from "./pages/LandingPage/LandingPage.jsx";
+import LoginPage from './pages/LoginPage/LoginPage.jsx';
+import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+const NavTabs = ["Assignments", "AddQuestion", "Questions"];
+const NavLinks = ["/professors/assignments", "/professors/addQuestion", "/professors/questions"];
 function App() {
 
   return (
@@ -11,6 +14,8 @@ function App() {
       <BrowserRouter>
         <Routes>
          <Route path="/" element={<LandingPage />} />
+          <Route path="/professorlogin" element={<LoginPage LoginType={"Professors"} />} />
+          <Route path="/studentlogin" element={<LoginPage LoginType={"Students"} />} />
         </Routes>
       </BrowserRouter>
     </>
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,8 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
